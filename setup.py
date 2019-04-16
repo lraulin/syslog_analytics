178558cecf577fd315ce8b08bbf137fe09951224
Lee Raulin <leeraulin@gmail.com>
2019-04-16 14:39:43 -0400

    Increment minor version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='syslog_analytics',
-                 version='0.0.2',
+                 version='0.0.3',
                  entry_points={
                      'console_scripts': ['syslog_analytics=syslog_analytics.main:main'],
                  },
